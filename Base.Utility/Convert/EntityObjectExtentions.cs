6964747dcd01cc78b733e8438dab2668873793c8
liuyuduen <375298432@qq.com>
2018-05-22 15:34:24 +0800

    ..

---
+++ Base.Utility/Convert/EntityObjectExtentions.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Base.Utility
+{
+
+    public static class EntityObjectExtentions
+    {
+        #region data to entity hepers
+
+        public static T ToObject<T>(this IDataReader dataReader, bool strict = true)
+        {
+            if (dataReader == null)
+            {
+                return default(T);
+            }
+
+            var cols = new List<string>();
+            for (int i = 0; i < dataReader.FieldCount; i++)
+            {
+                cols.Add(dataReader.GetName(i));
+            }
+
+            // Create a new type of the entity
+            Type t = typeof(T);
+            T returnObject = Activator.CreateInstance<T>();
+
+            PropertyInfo[] perperties = t.GetProperties();
+            foreach (PropertyInfo prop in perperties)
+            {
+                if (!prop.CanWrite)
+                    continue;
+
+                object val = null;
+                if (cols.Contains(prop.Name))
+                {
+                    val = dataReader[prop.Name];
+                }
+                else
+                {
+                    // 严格模式，无法在结果集中找到指定的列时抛出异常
+                    if (strict)
+                        throw new IndexOutOfRangeException(string.Format("Can not found column: {0}", prop.Name));
+                    else
+                        continue;
+                }
+
+                SetPropertyValue(returnObject, prop, val);
+            }
+
+            return returnObject;
+        }
+        public static T ToObject<T>(this DataRow dataRow)
+        {
+            if (dataRow == null)
+            {
+                return default(T);
+            }
+
+            // Create a new type of the entity
+            Type t = typeof(T);
+            T returnObject = Activator.CreateInstance<T>();
+
+            foreach (DataColumn col in dataRow.Table.Columns)
+            {
+                string colName = col.ColumnName;
+
+                // Look for the object's property with the columns name, ignore case
+                PropertyInfo pInfo = t.GetProperty(colName.ToLower(),
+                    BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                // did we find the property and can write ?
+                if (pInfo == null || !pInfo.CanWrite)
+                {
+                    continue;
+                }
+
+                object val = dataRow[colName];
+
+                SetPropertyValue(returnObject, pInfo, val);
+            }
+
+            // return the entity object with values
+            return returnObject;
+        }
+        public static T ToObject<T>(this DataTable dataTable)
+        {
+            if (dataTable == null || dataTable.Rows.Count == 0)
+            {
+                return default(T);
+            }
+
+            DataRow dataRow = dataTable.Rows[0];
+
+            return dataRow.ToObject<T>();
+        }
+        public static List<T> ToObjectCollection<T>(this DataTable dataTable)
+        {
+
+            if (dataTable != null && dataTable.Rows.Count > 0)
+            {
+                return dataTable.Rows.ToObjectCollection<T>();
+            }
+
+            return new List<T>();
+        }
+        public static List<T> ToObjectCollection<T>(this DataRowCollection dataRows)
+        {
+            List<T> collection = new List<T>();
+
+            if (dataRows != null && dataRows.Count > 0)
+            {
+                foreach (DataRow row in dataRows)
+                {
+                    collection.Add(row.ToObject<T>());
+                }
+            }
+
+            return collection;
+        }
+        private static void SetPropertyValue(object obj, PropertyInfo property, object value)
+        {
+            // is this a Nullable<> type
+            var underlyingType = Nullable.GetUnderlyingType(property.PropertyType);
+            if (underlyingType != null)
+            {
+                if (value is System.DBNull)
+                {
+                    value = null;
+                }
+                else
+                {
+                    // Convert the db type into the T we have in our Nullable<T> type
+                    value = Convert.ChangeType(value, underlyingType);
+                }
+            }
+            else
+            {
+                // Convert the db type into the type of the property in our entity
+                value = Convert.ChangeType(value, property.PropertyType);
+            }
+
+            // Set the value of the property with the value from the db
+            property.SetValue(obj, value, null);
+        }
+
+        #endregion
+    }
+}
---
+++ Base.Utility/Http/HttpHelper1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Base.Utility
+{
+    public static class HttpHelper1
+    {
+
+        static string smartProxyToken = ConfigHelper.AppSettings("SmartProxyToken");
+        internal static string HttpPostForSmartProxy(string url, string content)
+        {
+            HttpWebResponse response = null;
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                byte[] bytes = Encoding.UTF8.GetBytes(content);
+
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "application/json; charset=UTF-8";
+                httpWebRequest.Accept = "application/json";
+                httpWebRequest.ContentLength = bytes.Length;
+                long timestamp = GetUnixTimeStamp();
+                httpWebRequest.Headers.Add("timestamp", timestamp.ToString());
+                string signature = SHA256Encrypt(timestamp + smartProxyToken + timestamp);
+                httpWebRequest.Headers.Add("signature", signature);
+
+                //发送接口请求参数数据流
+                var requestStream = httpWebRequest.GetRequestStream();
+                //byte[] bytes = Encoding.UTF8.GetBytes(content);
+                requestStream.Write(bytes, 0, bytes.Length);
+                requestStream.Close();
+
+                //获取返回信息
+                //Utils.WriteLog("Signate", new string[] { timestamp.ToString(), timestamp + smartProxyToken + timestamp, signature });
+                response = (HttpWebResponse)httpWebRequest.GetResponse();
+                var myResponseStream = response.GetResponseStream();
+
+                string result = "";
+                //读取返回流
+                if (myResponseStream != null)
+                {
+                    var myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                    result = myStreamReader.ReadToEnd();
+                    //关闭请求连接
+                    myStreamReader.Close();
+                    myResponseStream.Close();
+                }
+                return result;
+            }
+            catch (WebException ex)
+            {
+                // 对于连接级别的错误，直接抛出异常
+                if (ex.InnerException != null && ex.InnerException is SocketException)
+                    throw ex;
+
+                // 注意，若捕获到 WebException 异常，则必须将异常中的Response对象关闭掉，否则将可能发生一些未预期的错误。
+                // 如果在抛出 WebException 异常后，不关闭异常的 Response 属性，已知的未预期错误有：
+                // ORA-24550: signal received: Unhandled exception: Code=e0434f4d Flags=1
+
+                response = (HttpWebResponse)ex.Response;
+                var errMsg = ex.Message + "\r\n" + GetErroMsgFromResponse(response);
+                throw new Exception("HttpPost异常：\r\n" + errMsg);//NoticeException("HttpPost异常：\r\n" + errMsg);
+
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                    response = null;
+                }
+            }
+        }
+
+
+        private static string GetErroMsgFromResponse(HttpWebResponse response)
+        {
+            string errMsg = string.Empty;
+            if (response == null)
+            {
+                return errMsg;
+            }
+            var data = new byte[0];
+            if (response != null)
+            {
+                using (var s = response.GetResponseStream())
+                {
+                    Stream stream;
+                    switch (response.ContentEncoding.ToUpperInvariant())
+                    {
+                        case "GZIP":
+                            stream = new GZipStream(s, CompressionMode.Decompress);
+                            break;
+                        case "DEFLATE":
+                            stream = new DeflateStream(s, CompressionMode.Decompress);
+                            break;
+
+                        default:
+                            stream = s;
+                            break;
+                    }
+
+                    using (stream)
+                    {
+                        data = new byte[response.ContentLength > 0 ? response.ContentLength : 0];
+                        byte[] buffer = new byte[1024]; // HACK：每次读取的字节数
+                        int copied = 0;
+                        int n = 0;
+                        while ((n = stream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            if (data.Length < copied + n)
+                                Array.Resize(ref data, copied + n);
+
+                            Array.Copy(buffer, 0, data, copied, n);
+                            copied += n;
+                        }
+                    }
+                }
+            }
+
+            return Encoding.UTF8.GetString(data);
+
+        }
+
+        internal static string HttpPostForSmartProxy(string url, byte[] bytes, WebHeaderCollection headers)
+        {
+            HttpWebResponse response = null;
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                //byte[] bytes = Encoding.UTF8.GetBytes(content);
+
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "text/html; charset=utf-8";
+                httpWebRequest.Accept = "application/json";
+                httpWebRequest.ContentLength = bytes.Length;
+                long timestamp = GetUnixTimeStamp();
+                httpWebRequest.Headers.Add("timestamp", timestamp.ToString());
+                string signature = SHA256Encrypt(timestamp + smartProxyToken + timestamp);
+                httpWebRequest.Headers.Add("signature", signature);
+
+                httpWebRequest.Headers.Add(headers);
+                //Utils.WriteLog("Signate", new string[] { timestamp.ToString(), timestamp + smartProxyToken + timestamp, signature });
+                //发送接口请求参数数据流
+                var requestStream = httpWebRequest.GetRequestStream();
+                //byte[] bytes = Encoding.UTF8.GetBytes(content);
+                requestStream.Write(bytes, 0, bytes.Length);
+                requestStream.Close();
+
+                //获取返回信息
+                response = (HttpWebResponse)httpWebRequest.GetResponse();
+                var myResponseStream = response.GetResponseStream();
+
+                string result = "";
+                //读取返回流
+                if (myResponseStream != null)
+                {
+                    var myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                    result = myStreamReader.ReadToEnd();
+                    //关闭请求连接
+                    myStreamReader.Close();
+                    myResponseStream.Close();
+                }
+                return result;
+            }
+            catch (WebException ex)
+            {
+                // 对于连接级别的错误，直接抛出异常
+                if (ex.InnerException != null && ex.InnerException is SocketException)
+                    throw ex;
+
+                // 注意，若捕获到 WebException 异常，则必须将异常中的Response对象关闭掉，否则将可能发生一些未预期的错误。
+                // 如果在抛出 WebException 异常后，不关闭异常的 Response 属性，已知的未预期错误有：
+                // ORA-24550: signal received: Unhandled exception: Code=e0434f4d Flags=1
+
+                response = (HttpWebResponse)ex.Response;
+                var errMsg = ex.Message + "\r\n" + GetErroMsgFromResponse(response);
+                throw new Exception("HttpPost异常：\r\n" + errMsg);
+
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                    response = null;
+                }
+            }
+
+        }
+
+        internal static string HttpPost(string url, string content)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            byte[] bytes = Encoding.UTF8.GetBytes(content);
+
+            httpWebRequest.Method = "POST";
+            httpWebRequest.ContentType = "application/json; charset=UTF-8";
+            httpWebRequest.Accept = "application/json";
+            httpWebRequest.ContentLength = bytes.Length;
+            //发送接口请求参数数据流
+            var requestStream = httpWebRequest.GetRequestStream();
+            //byte[] bytes = Encoding.UTF8.GetBytes(content);
+            requestStream.Write(bytes, 0, bytes.Length);
+            requestStream.Close();
+
+            //获取返回信息
+            var response = (HttpWebResponse)httpWebRequest.GetResponse();
+            var myResponseStream = response.GetResponseStream();
+
+            string result = "";
+            //读取返回流
+            if (myResponseStream != null)
+            {
+                var myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                result = myStreamReader.ReadToEnd();
+                //关闭请求连接
+                myStreamReader.Close();
+                myResponseStream.Close();
+            }
+            return result;
+        }
+
+        internal static string HttpGet(string url)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            //设置HttpWebRequest头信息
+            httpWebRequest.Method = "GET";
+            httpWebRequest.Timeout = 600000;
+            httpWebRequest.ServicePoint.Expect100Continue = false;
+            httpWebRequest.CachePolicy = new System.Net.Cache.HttpRequestCachePolicy(System.Net.Cache.HttpRequestCacheLevel.NoCacheNoStore);
+            httpWebRequest.ContentType = "application/json; charset=UTF-8";
+            httpWebRequest.Accept = "application/json";
+
+            //获取响应输入流
+            WebResponse webResponse = httpWebRequest.GetResponse();
+            Stream responseStream = webResponse.GetResponseStream();
+
+            string result = "";
+            //读取返回流
+            if (responseStream != null)
+            {
+                var myStreamReader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                result = myStreamReader.ReadToEnd();
+
+                //关闭请求连接  
+                myStreamReader.Close();
+                responseStream.Close();
+            }
+            return result;
+        }
+
+        internal static string SHA256Encrypt(string str)
+        {
+            StringBuilder pwd = new StringBuilder();
+            using (var hash = SHA256.Create())
+            {
+                byte[] s = hash.ComputeHash(Encoding.UTF8.GetBytes(str));
+                for (int i = 0; i < s.Length; i++)
+                    pwd.Append(s[i].ToString("X2"));
+            }
+            return pwd.ToString();
+
+        }
+        internal static long GetUnixTimeStamp()
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
+            DateTime nowTime = DateTime.Now;
+            long unixTime = (long)Math.Round((nowTime - startTime).TotalSeconds, MidpointRounding.AwayFromZero);
+            return unixTime;
+        }
+
+    }
+}
